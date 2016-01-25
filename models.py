3b1dcd7cadfe5e9a78c846dfeb4dce82becd7a89
Justin Stewart <justinstewart.la@gmail.com>
2016-01-25 12:17:53 -0500

    editing movie class docsrings

--- models.py
+++ models.py
@@ -5,7 +5,7 @@ class Movie(object):
 		being scraped from box office mojo"""
 
 	def __init__(self, name, url= None):
-        """Return a Movie object whose name is *name* and can be found
-        at the box office mojo url given."""
-        self.name = name
-        self.url = url
+	    """Return a Movie object whose name is *name* and can be found
+	    at the box office mojo url given."""
+	    self.name = name
+	    self.url = url
